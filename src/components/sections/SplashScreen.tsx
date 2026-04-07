0ecb19b8749a67fbc9284226fd4f760735b6f2c0
Oualid Chebab <oualid@freedommobile.ca>
2026-04-07 10:28:53 -0400

    Mobile: safety zone 10px bottom

--- src/components/sections/SplashScreen.tsx
+++ src/components/sections/SplashScreen.tsx
@@ -172,7 +172,7 @@ export function SplashScreen({ onClose, autoCloseDelay = 10000 }: SplashScreenPr
             <div className="w-[70%] h-[1px] bg-white/20 mx-auto md:hidden flex-shrink-0" />
 
             {/* Right - Text Content - centered, shifted left on desktop */}
-            <div className="splash-screen__text flex flex-col justify-start md:justify-center items-center text-center px-6 md:px-16 lg:px-24 pt-3 pb-[5px] md:py-0 w-full md:w-[40%] md:-ml-10 flex-shrink-0">
+            <div className="splash-screen__text flex flex-col justify-start md:justify-center items-center text-center px-6 md:px-16 lg:px-24 pt-3 pb-[10px] md:py-0 w-full md:w-[40%] md:-ml-10 flex-shrink-0">
               {/* Samsung Logo */}
               <Image
                 src="/images/samsung-logo-white.webp"
